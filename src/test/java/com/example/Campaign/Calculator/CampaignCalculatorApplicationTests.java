91bef7718d18ad24b2ea2be9dfdea50c3b52e6ae
Valer44ik <Valerij.Goncharov@gmail.com>
2024-01-29 03:36:31 +0200

    Made a pilot and mech creation pages, started to work on adding mech to a pilot page.

---
+++ src/test/java/com/example/Campaign/Calculator/CampaignCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Campaign.Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
